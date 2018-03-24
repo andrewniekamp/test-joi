5b4afc62342ab575d10504593b0f03a5a4223d8c
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-03-24 16:37:12 -0400

    Add nodemon and basic route to test

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/details', (req, res) => res.send('Here are the details!'))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
